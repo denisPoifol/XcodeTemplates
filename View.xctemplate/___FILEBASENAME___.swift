307c1bb62b1f76e88c7bfc03c0a946b23db24a4d
Denis Poifol <denis.poifol@gmail.com>
2017-08-05 13:53:43 +0200

    Create View template
    This template create a view swift class an empty viewModel and an also
    empty viewModelMapper

---
+++ View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAME___: ___VARIABLE_SuperClass___ {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    // MARK: - ___FILENAME___
+
+    func configure(with viewModel: ___VARIABLE_ViewModel___) {
+	// TODO: (___FULLUSERNAME___) Configure view
+    }
+
+    // MARK: - Private methods
+
+    private func setup() {
+        setupLayout()
+	// TODO: (___FULLUSERNAME___) Setup subviews
+    }
+
+    private func setupLayout() {
+	// TODO: (___FULLUSERNAME___) Setup layout
+    }
+}
