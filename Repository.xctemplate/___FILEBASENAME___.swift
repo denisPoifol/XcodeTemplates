6dc94b4fa15cc958334c42df32247802c795cacf
Denis Poifol <denis.poifol@gmail.com>
2017-08-06 14:00:34 +0200

    Create Repository template

---
+++ Repository.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+protocol ___FILEBASENAME___ {
+    // TODO: (___FULLUSERNAME___) Add requirements to ___FILEBASENAME___ 
+}
---
+++ Repository.xctemplate/___VARIABLE_ImplementingClass___.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+class ___VARIABLE_ImplementingClass___: ___VARIABLE_productName___ {
+
+    // MARK: LifeCycle
+
+    init() {
+        // TODO: (___FULLUSERNAME___) Initialize with repository dependencies
+    }
+
+    // MARK: - ___VARIABLE_productName___
+
+}
