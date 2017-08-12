576fb71d4800854e206c008a524fccf3b96d38b4
Denis Poifol <denis.poifol@gmail.com>
2017-08-12 11:05:20 +0200

    Create Coordinator template

---
+++ Coordinator.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAME___: Coordinator {
+
+    let navigationController: UINavigationController
+
+    // MARK: - LifeCycle
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: - ___FILEBASENAME___
+}
